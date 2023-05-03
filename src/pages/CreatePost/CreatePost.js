b34dac6b418de96dd475773b5ae4211f868c6fe1
Ruan Sousa <103215747+RuanSousa25@users.noreply.github.com>
2023-05-03 10:47:33 -0300

    entrarão na minha casa programarão meu blog

--- src/pages/CreatePost/CreatePost.js
+++ src/pages/CreatePost/CreatePost.js
@@ -3,19 +3,53 @@ import styles from "./CreatePost.module.css";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuthValue } from "../../contexts/AuthContext";
-
+import { useInsertDocument } from "../../hooks/useInsertDocument";
 const CreatePost = () => {
   const [title, setTitle]= useState("");
   const [image, setImage] = useState("");
   const [body,setBody]= useState("");
   const [tags, setTags] = useState([]);
-  const [formError, setFormError] = useState(null);
-
+  const [formError, setFormError] = useState("");
+  const {user} = useAuthValue();
+  const {insertDocument, response} = useInsertDocument("posts");
+  const navigate = useNavigate();
   const handleSubmit =(e)=>{
     e.preventDefault();
+    setFormError("");
+
+    //validate img URL
+    try{
+      new URL(image);
+    }
+    catch(e){
+      setFormError("A imagem precisa ser uma URL")
+    }
+    //criar o array de tags
+    const tagsArray = tags.split(",").map((tag)=>tag.trim().toLowerCase());
+    //checar todos os valores
+    if(!title || !image || !tags || !body){
+      setFormError("por favor, preencha todos os campos!");
+    }
+    if(formError){
+      return
+    }
+
+
+    insertDocument({
+      title,
+      image,
+      body,
+      tagsArray,
+      uid: user.uid,
+      createdBy: user.displayName
+    })
+
+
+    //redirect to home page
+      navigate("/");
   }
   return (
-    <div>
+    <div className={styles.create_post}>
         <h1>CreatePost</h1>
         <p>O que está pensando?</p>
         <form onSubmit={handleSubmit}>
@@ -35,7 +69,7 @@ const CreatePost = () => {
                 type="text" 
                 name="image" 
                 required 
-                placeholder="Insira uma imagem que representa seu post" 
+                placeholder="Insira o URL de uma imagem que representa seu post" 
                 onChange={(e)=>setImage(e.target.value)} 
                 value={image}></input>
             </label>
@@ -55,9 +89,13 @@ const CreatePost = () => {
                 name="image" 
                 required 
                 placeholder="Insira as tags separadas por vírgulas" 
-                onChange={(e)=>setImage(e.target.value)} 
-                value={image}></input>
+                onChange={(e)=>setTags(e.target.value)} 
+                value={tags}></input>
             </label>
+            {/* <button className="btn">Postar</button> */}
+             {!response.loading && <button className="btn">Postar</button>}
+          {response.loading && <button className="btn" disabled>Aguarde</button>}
+          {formError && <p className="error">{formError}</p>}
         
         </form>
     </div>
